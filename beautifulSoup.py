0d98d866f11f65b0e95262f13de963d4abef512e
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-08-22 16:29:40 +0900

    [add]beautifulsoupでスクレイピング&&CSV化実装

---
+++ beautifulSoup.py
@@ -0,0 +1,49 @@
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+import json
+import csv
+from datetime import datetime as dt
+
+QIITA_URL = 'https://qiita.com/'
+
+def get_trend_items():
+    req = Request(QIITA_URL)
+    with urlopen(req) as res:
+        body = res.read()
+        soup = BeautifulSoup(body, 'html.parser')
+        target_div = soup.select('div[data-hyperapp-app="Trend"]')[0]
+        trend_items = json.loads(target_div.get('data-hyperapp-props'))
+    return trend_items
+
+def csv_writer(trend_items):
+    with open('./qiitaTopPage.csv', 'a') as f:
+        for values in trend_items['trend']['edges']:
+            for key, val in values.items():
+                if key == 'node':
+                    tmp_dick = {}
+                    for c_key, c_val in val.items():
+                        try:
+                            if c_key == 'createdAt':
+                                #一旦文字列を減らしてdata型で読み込めるようにする　2020-08-21T02:16:52Z  2020-08-21
+                                c_val = dt.fromisoformat(c_val[:-10])
+                                c_val = c_val.strftime('%Y-%m-%d')
+
+                            if type(c_val) is not dict:
+                                tmp_dick[c_key] = c_val
+                            else:
+                                for c2_key, c2_val in   c_val.items():
+                                    tmp_dick[c2_key] = c2_val
+                        except:
+                           continue
+                    try:
+                        writer = csv.DictWriter(f, tmp_dick)
+                        #writer.writeheader()
+                        writer.writerow(tmp_dick)
+                    except:
+                        print("書き込み失敗")
+
+result = get_trend_items()
+
+csv_writer(result)
+
+

dc5317f98f181c31dc0e0b41bc5466962f702406
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-10-22 00:50:31 +0900

    [add]urlスクレイパー作成

--- Selenium/link_in_a_webpage.py
+++ Selenium/link_in_a_webpage.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import chromedriver_binary 
 
 # Method to find the main website from the given URL
 def main_website(url) -> str:
@@ -8,21 +9,21 @@ def main_website(url) -> str:
 
 # Method to check if the keywords of scrapped link matches with the main website
 def present(url:str, web_main:str) -> bool:
-     ok_flag = False
-     for i in range(0, len(url)-len(web_main)+1):
-         if url[i:i+len(web_main)]:
-             ok_flag = True
-             break
+    ok_flag = False
+    for i in range(0, len(url)-len(web_main)+1):
+        if url[i:i+len(web_main)]:
+            ok_flag = True
+            break
     return ok_flag
 
 # Method to check all links in a webpage and categorise them as External or Internal Links
 def links(url:str):
     # Running the driver in headless mode
-    options = webdriver.ChromeOptions()
-    options.add_argument("headless")
+    # options = webdriver.ChromeOptions()
+    # options.add_argument("headless")
 
     # Change the driver_path to where your chrome driver is installed
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()#(options=options)
 
     links = ""
     try:
@@ -34,11 +35,12 @@ def links(url:str):
         driver.quit()
 
         # Using beautiful soup to filter the scraped data
-        soup = BeautifulSoup(html, "lxml")  # get element(fast than "html.parser")
+        soup = BeautifulSoup(html, "html.parser")
         links = soup.find_all("a")
         web_main = main_website(url)
         internal_links = []
         external_links = []
+
         for link in links:
             try:
                 final_url = ""
@@ -49,3 +51,33 @@ def links(url:str):
                     internal_links.append(final_url)
                 elif x == '#':
                     final_url = url + '/' + link['href']
+                    internal_links.append(final_url)
+                else:
+                    check = present(link['href'], web_main)
+                    if check:
+                        final_url = link['href']
+                        internal_links.append(final_url)
+                    else:
+                        final_url = link['href']
+                        external_links.append(final_url)
+            except:
+                print("working on your request....")
+        
+        print('Links assosciated to the webpage: ')
+        print('\n\tInternal Links :')
+        if len(external_links) == 0:
+            print("\t\t","No Internal Links!")
+        for i in range(1, len(internal_links)+1):
+            print("\t\t", internal_links[i-1])
+        
+        print("\n\tExternal Links :")
+        if len(external_links) == 0:
+            print('\t\t', 'No External Links!')
+        for i in range(1, len(external_links)+1):
+            print('\t\t', external_links[i-1])
+    except:
+        print('Enter a valid webpage URL!')
+
+if __name__ == "__main__":
+    url = input("Enter the url of the webpage whose all links are to be grouped(As External or Internal) : ")
+    links(url)

da75700e03092d9fbda532acac1fa3994b34a87f
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2020-10-27 16:59:15 +0900

    [update]天気情報取得APIサンプル実装完了

--- slack_bot/wheather_test/notify_wheather.py
+++ slack_bot/wheather_test/notify_wheather.py
@@ -4,36 +4,43 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-input_city = input('天気を知りたい都市名を入力ください 例：東京, 大阪...\n')
+def main():
+    input_city = input('天気を知りたい都市名を入力ください 例：東京, 大阪...\n')
 
-# dotenvを用いた.envからのAPI keyの読み出し
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+    # dotenvを用いた.envからのAPI keyの読み出し
+    dotenv_path = join(dirname(__file__), '.env')
+    load_dotenv(dotenv_path)
 
-WHEATHER_KEY = os.environ['WHEATHER_KEY']
+    WHEATHER_KEY = os.environ['WHEATHER_KEY']
 
-city_trance = {
-    '東京':'Tokyo',
-    '大阪':'Osaka',
-    '古賀':'Koga',
-    '船橋':'Funabashi'
-}
+    city_trance = {
+        '東京':'Tokyo',
+        '大阪':'Osaka',
+        '古賀':'Koga',
+        '船橋':'Funabashi'
+    }
 
-if input_city in city_trance:
-    city = city_trance[input_city]
-else:
-    print('大都市を入力ください')
+    if input_city in city_trance:
+        city = city_trance[input_city]
+    elif input_city == 'exit' or input_city == 'no':
+        print('終了します!!')
+        exit()
+    else:
+        print('大都市を入力ください')
+        main()
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?units=metric&q={city}&appid={WHEATHER_KEY}"
+
+    response = requests.get(url)
+
+    data = response.json()
+    #print(data)
+
+    # jsonデータがきれいに整形されてコンソールに出力される
+    jsontext = json.dumps(data,indent=4)
+    print(jsontext)
     exit()
 
-url = f"http://api.openweathermap.org/data/2.5/weather?units=metric&q={city}&appid={WHEATHER_KEY}"
 
-response = requests.get(url)
-
-data = response.json()
-#print(data)
-
-# きれいに整形されてコンソールに出力される
-jsontext = json.dumps(data,indent=4)
-print(jsontext)
-
-
+if __name__ == "__main__":
+    main()
